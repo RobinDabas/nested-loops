4f8f080916e4130c2544be91907d328b63831417
ROBERTO D <robindabas@hotmail.com>
2025-05-20 19:37:29 -0400

    use of loops

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ for (var i = 1; i <= 10; ++i) {
     console.log(i + ' x ' + j + ' = ' + (i * j))
   }
 } console.log('Vos sos lo máximo habibi. ¡Seguí pa llante!')
+
+
+var cubes = 'QAHTAN.505.LIBYA';
+
+for (var i = 0; i <= 16; ++i) {
+  var styles = 'font-size: 40px; border-radius: 10px, border: 10px solid black; background: #ff0000; color: white'
+  console.log('%c' + cubes[i], styles)
+}
+
